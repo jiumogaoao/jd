e4629eeb99112c87170804aeac23a39c350ab80e
jiumogaoao <394127821@qq.com>
2020-02-16 10:03:36 +0800

    1
    1

---
+++ pages/detail/detail.vue
@@ -0,0 +1,207 @@
+<template>
+	<view class="content">
+		<view class="tabbar">
+			<view class="tab hl">产品详情<view class="line"></view></view>
+			<view class="tab">使用说明<view class="line"></view></view>
+		</view>
+		<scroll-view class="scroll">
+			<swiper class="swiper" :indicator-dots="true" :autoplay="true">
+				<swiper-item>
+				   <image class="point" src="/static/logo.png"></image>
+				</swiper-item>
+				<swiper-item>
+				   <image class="point" src="/static/logo.png"></image>
+				</swiper-item>
+				<swiper-item>
+				   <image class="point" src="/static/logo.png"></image>
+				</swiper-item>
+				<swiper-item>
+				   <image class="point" src="/static/logo.png"></image>
+				</swiper-item>
+				<swiper-item>
+				   <image class="point" src="/static/logo.png"></image>
+				</swiper-item>
+			</swiper>
+			<view class="timeFrame">
+				<view class="time">限时抢购</view>
+				<view class="time">已结束</view>
+			</view>
+			<view class="mainFrame">
+				<view class="title">
+					库存告急！中午12点准时下架！【全国京东物流包邮72小时内发货】原森态更好的防护~199元抢门市价399元原生态防雾霾PM2.5
+				防尘防病菌KN95透气电动防护口罩~手慢无~
+				</view>
+				<view class="price"><text style="font-size: 26rpx;">&yen;</text>199</view>
+				<view class="fan">
+					<view class="left">超返</view>
+					<view class="right">&yen;20</view>
+				</view>
+				<view class="number">已售:821 库存:73</view>
+			</view>
+		</scroll-view>
+		<view class="bottom">
+			<view class="index" @click="goHome">首页</view>
+			<view class="button">分享</view>
+			<view class="button">抢购</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			};
+		},
+		methods:{
+			goHome(){
+				uni.reLaunch({
+					url:'/pages/index/index'
+				})
+			}
+		}
+	}
+</script>
+
+<style lang="scss">
+	.content {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		position:fixed;
+		top:0;
+		left:0;
+		right:0;
+		bottom:0;
+		background-color: #f3f2f2;
+		.tabbar{
+			width:750rpx;
+			height: 88rpx;
+			background-color:#fff;
+			display: flex;
+			flex-shrink: 0;
+			.tab{
+				width:50%;
+				height: 100%;
+				position: relative;
+				line-height: 88rpx;
+				font-size: 28rpx;
+				text-align: center;
+				.line{
+					width:10%;
+					height: 2px;
+					position: absolute;
+					left: 45%;
+					bottom:2rpx;
+					background-color:#f8ab0c;
+					display: none;
+				}
+				&.hl{
+					font-weight: bolder;
+					.line{display: block;}
+				}
+			}
+		}
+		.scroll{
+			width: 750rpx;
+			height: 10px;
+			flex-grow: 1;
+			overflow: hidden;
+			.swiper{
+				width:750rpx;
+				height: 346rpx;
+				.point{
+					width:750rpx;
+					height: 346rpx;
+				}
+			}
+			.timeFrame{
+				width:750rpx;
+				height: 86rpx;
+				background-color: #fd3f6b;
+				display: flex;
+				align-items: center;
+				justify-content: space-between;
+				padding: 0 22rpx;
+				.time{
+					font-size: 28rpx;
+					color: #fff;
+				}
+			}
+			.mainFrame{
+				width: 100%;
+				padding: 32rpx 30rpx 50rpx 20rpx;
+				background-color: #fff;
+				.title{
+					font-size: 28rpx;
+					line-height: 1.5;
+				}
+				.price{
+					font-size: 32rpx;
+					color: #f46838;
+					margin-top: 50rpx;
+				}
+				.fan{
+					height: 37rpx;
+					padding: 4rpx;
+					background-color: #f46838;
+					display: flex;
+					align-items: center;
+					border-radius: 6rpx;
+					width: fit-content;
+					margin-top: 30rpx;
+					.left{
+						padding: 0 5rpx;
+						font-size: 22rpx;
+						color: #fff;
+						line-height: 29rpx;
+						flex-shrink: 0;
+					}
+					.right{
+						padding: 0 5rpx;
+						font-size: 22rpx;
+						color: #f46838;
+						line-height: 29rpx;
+						flex-shrink: 0;
+						background-color: #fff;
+					}
+				}
+				.number{
+					font-size: 23rpx;
+					color: #aaa;
+					text-align: right;
+					margin-top: 38rpx;
+				}
+			}
+		}
+		.bottom{
+			width: 750rpx;
+			height: 80rpx;
+			flex-shrink: 0;
+			display: flex;
+			align-items: center;
+			justify-content: space-between;
+			.index{
+				width:30%;
+				height: 80rpx;
+				line-height: 80rpx;
+				text-align: center;
+				font-size: 42rpx;
+				border: 1px solid #e2c168;
+				background-color: #fff;
+			}
+			.button{
+				width:35%;
+				height: 80rpx;
+				line-height: 80rpx;
+				text-align: center;
+				font-size: 42rpx;
+				background-color: #ff0000;
+				color: #fff;
+				border: 1px solid #e2c168;
+				border-left: 0;
+			}
+		}
+	}
+</style>
--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,9 +1,154 @@
 <template>
 	<view class="content">
-		<image class="logo" src="/static/logo.png"></image>
-		<view class="text-area">
-			<text class="title">{{title}}</text>
+		<view class="head">
+			<view class="back"></view>
+			<view class="search"></view>
 		</view>
+		<scroll-view scroll-x="true" class="tabbar">
+			<view class="tabFrame">
+				<view class="tab hl">精选<view class="line"></view></view>
+				<view class="tab">家居用品<view class="line"></view></view>
+				<view class="tab">食品饮料<view class="line"></view></view>
+				<view class="tab">母婴<view class="line"></view></view>
+				<view class="tab">美妆护肤<view class="line"></view></view>
+				<view class="tab">猫猫狗狗<view class="line"></view></view>
+				<view class="tab">鱼虾蟹<view class="line"></view></view>
+			</view>
+		</scroll-view>
+		<scroll-view scroll-y="true" class="bottom">
+			<view class="point" @click="goDetail">
+				<image class="pic" src="/static/logo.png"></image>
+				<view class="right">
+					<view class="pointTop">
+						<view class="title">
+							<image src="/static/1.png" class="jd"></image>
+							<text>[抢！拍3件仅199元] 味库 越南活冻...</text>
+						</view>
+						<image class="smallShare" src="/static/2.png"></image>
+					</view>
+					<view class="pointBottom">
+						<view class="priceFrame">
+							<view class="price">&yen;<text style="font-size: 28rpx;">85</text> 抢购</view>
+							<view class="other">
+								<view class="coupon">3元券</view>
+								<view class="earn">赚 &yen;0.9</view>
+							</view>
+						</view>
+						<image class="bigShare" src="/static/3.png"></image>
+					</view>
+				</view>
+			</view>
+			<view class="point" @click="goDetail">
+				<image class="pic" src="/static/logo.png"></image>
+				<view class="right">
+					<view class="pointTop">
+						<view class="title">
+							<image src="/static/1.png" class="jd"></image>
+							<text>[抢！拍3件仅199元] 味库 越南活冻...</text>
+						</view>
+						<image class="smallShare" src="/static/2.png"></image>
+					</view>
+					<view class="pointBottom">
+						<view class="priceFrame">
+							<view class="price">&yen;<text style="font-size: 28rpx;">85</text> 抢购</view>
+							<view class="other">
+								<view class="coupon">3元券</view>
+								<view class="earn">赚 &yen;0.9</view>
+							</view>
+						</view>
+						<image class="bigShare" src="/static/3.png"></image>
+					</view>
+				</view>
+			</view>
+			<view class="point" @click="goDetail">
+				<image class="pic" src="/static/logo.png"></image>
+				<view class="right">
+					<view class="pointTop">
+						<view class="title">
+							<image src="/static/1.png" class="jd"></image>
+							<text>[抢！拍3件仅199元] 味库 越南活冻...</text>
+						</view>
+						<image class="smallShare" src="/static/2.png"></image>
+					</view>
+					<view class="pointBottom">
+						<view class="priceFrame">
+							<view class="price">&yen;<text style="font-size: 28rpx;">85</text> 抢购</view>
+							<view class="other">
+								<view class="coupon">3元券</view>
+								<view class="earn">赚 &yen;0.9</view>
+							</view>
+						</view>
+						<image class="bigShare" src="/static/3.png"></image>
+					</view>
+				</view>
+			</view>
+			<view class="point" @click="goDetail">
+				<image class="pic" src="/static/logo.png"></image>
+				<view class="right">
+					<view class="pointTop">
+						<view class="title">
+							<image src="/static/1.png" class="jd"></image>
+							<text>[抢！拍3件仅199元] 味库 越南活冻...</text>
+						</view>
+						<image class="smallShare" src="/static/2.png"></image>
+					</view>
+					<view class="pointBottom">
+						<view class="priceFrame">
+							<view class="price">&yen;<text style="font-size: 28rpx;">85</text> 抢购</view>
+							<view class="other">
+								<view class="coupon">3元券</view>
+								<view class="earn">赚 &yen;0.9</view>
+							</view>
+						</view>
+						<image class="bigShare" src="/static/3.png"></image>
+					</view>
+				</view>
+			</view>
+			<view class="point" @click="goDetail">
+				<image class="pic" src="/static/logo.png"></image>
+				<view class="right">
+					<view class="pointTop">
+						<view class="title">
+							<image src="/static/1.png" class="jd"></image>
+							<text>[抢！拍3件仅199元] 味库 越南活冻...</text>
+						</view>
+						<image class="smallShare" src="/static/2.png"></image>
+					</view>
+					<view class="pointBottom">
+						<view class="priceFrame">
+							<view class="price">&yen;<text style="font-size: 28rpx;">85</text> 抢购</view>
+							<view class="other">
+								<view class="coupon">3元券</view>
+								<view class="earn">赚 &yen;0.9</view>
+							</view>
+						</view>
+						<image class="bigShare" src="/static/3.png"></image>
+					</view>
+				</view>
+			</view>
+			<view class="point" @click="goDetail">
+				<image class="pic" src="/static/logo.png"></image>
+				<view class="right">
+					<view class="pointTop">
+						<view class="title">
+							<image src="/static/1.png" class="jd"></image>
+							<text>[抢！拍3件仅199元] 味库 越南活冻...</text>
+						</view>
+						<image class="smallShare" src="/static/2.png"></image>
+					</view>
+					<view class="pointBottom">
+						<view class="priceFrame">
+							<view class="price">&yen;<text style="font-size: 28rpx;">85</text> 抢购</view>
+							<view class="other">
+								<view class="coupon">3元券</view>
+								<view class="earn">赚 &yen;0.9</view>
+							</view>
+						</view>
+						<image class="bigShare" src="/static/3.png"></image>
+					</view>
+				</view>
+			</view>
+		</scroll-view>
 	</view>
 </template>
 
@@ -18,35 +163,169 @@
 
 		},
 		methods: {
-
+			goDetail(){
+				uni.navigateTo({
+					url:'/pages/detail/detail'
+				})
+			}
 		}
 	}
 </script>
 
-<style>
+<style lang="scss">
 	.content {
 		display: flex;
 		flex-direction: column;
 		align-items: center;
-		justify-content: center;
-	}
-
-	.logo {
-		height: 200rpx;
-		width: 200rpx;
-		margin-top: 200rpx;
-		margin-left: auto;
-		margin-right: auto;
-		margin-bottom: 50rpx;
-	}
-
-	.text-area {
-		display: flex;
-		justify-content: center;
-	}
-
-	.title {
-		font-size: 36rpx;
-		color: #8f8f94;
+		position:fixed;
+		top:0;
+		left:0;
+		right:0;
+		bottom:0;
+		.head{
+			width:750rpx;
+			height:75rpx;
+			background-image: url('~@/static/0.png');
+			background-size: 750rpx 75rpx;
+			position: relative;
+			flex-shrink: 0;
+			.back{
+				width:25rpx;
+				height:30rpx;
+				position: absolute;
+				top:23rpx;
+				left:33rpx;
+			}
+			.search{
+				width:33rpx;
+				height:30rpx;
+				position: absolute;
+				top:23rpx;
+				right:27rpx;
+			}
+		}
+		.tabbar{
+			width:750rpx;
+			height: 64rpx;
+			background-color: #fff;
+			flex-shrink: 0;
+			overflow: hidden;
+			.tabFrame{
+				height: 64rpx;
+				display: flex;
+				flex-wrap: nowrap;
+				align-items: center;
+				padding: 0 37rpx;
+				.tab{
+					font-size: 24rpx;
+					padding: 0 37rpx;
+					flex-shrink: 0;
+					position: relative;
+					.line{
+						width:100%;
+						height: 2rpx;
+						background-color: #333;
+						position: absolute;
+						bottom:-9rpx;
+						left:0rpx;
+						display: none;
+					}
+					&.hl{
+						font-weight: bold;
+						.line{
+							display:block;
+						}
+					}
+				}
+			}
+		}
+		.bottom{
+			width:750rpx;
+			height: 10px;
+			flex-grow: 1;
+			background-color: #f2f1f5;
+			padding-top: 8rpx;
+			.point{
+				height:227rpx;
+				width:703rpx;
+				background-color: #fff;
+				border-radius: 10rpx;
+				margin: auto;
+				margin-bottom: 17rpx;
+				padding: 5rpx 0 5rpx 17rpx;
+				display: flex;
+				align-items: center;
+				justify-content: space-between;
+				.pic{
+					width:217rpx;
+					height:217rpx;
+					flex-shrink: 0;
+				}
+				.right{
+					width:403rpx;
+					height: 100%;
+					display: flex;
+					flex-direction: column;
+					justify-content: space-between;
+					.pointTop{
+						width:100%;
+						display: flex;
+						align-items: center;
+						justify-content: space-between;
+						padding-top: 14rpx;
+						.title{
+							width:260rpx;
+							font-size: 23rpx;
+							line-height: 1.5;
+							.jd{
+								width:29rpx;
+								height:26rpx;
+								margin-right: 20rpx;
+							}
+						}
+						.smallShare{
+							width:101rpx;
+							height:34rpx;
+						}
+					}
+					.pointBottom{
+						display: flex;
+						justify-content: space-between;
+						align-items: center;
+						.priceFrame{
+							.price{
+								font-size: 18rpx;
+								color: red;
+							}
+							.other{
+								display: flex;
+								align-items: center;
+								.coupon{
+									width:128rpx;
+									height: 30rpx;
+									margin-right: 10rpx;
+									background-size: 128rpx 30rpx;
+									line-height: 30rpx;
+									text-align: center;
+									font-size: 20rpx;
+									color: #fff;
+									background-image: url('~@/static/4.png');
+								}
+								.earn{
+									padding: 3rpx 18rpx;
+									background-color: #feecc5;
+									font-size: 18rpx;
+									color: #75512a;
+								}
+							}
+						}
+						.bigShare{
+							width:127rpx;
+							height:74rpx;
+						}
+					}
+				}
+			}
+		}
 	}
 </style>
